52aff41c3e1b81fc9d40aa411377a551a6b28c3c
itc-s25011 <s25011@std.it-college.ac.jp>
2026-05-15 12:40:31 +0900

    課題

---
+++ src/d48/Q22.java
@@ -0,0 +1,12 @@
+package src d48;
+enum Connection {OFFLINE, ONLINE}
+
+public class Q22 {
+    public static void main(String[]args){
+        connection s = Connection.OFFLINE;
+        if (s == Connection.OFFLINE){
+            s = Connection.ONLINE
+        }
+        System.out.println(s);
+    }
+}
