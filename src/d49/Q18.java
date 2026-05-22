51359349ef44ac6ccefb316a364471b6c60957c5
itc-s25011 <s25011@std.it-college.ac.jp>
2026-05-22 12:15:23 +0900

    課題

---
+++ src/d49/Q18.java
@@ -0,0 +1,12 @@
+package d49;
+
+public class Q18 {
+    public static void main(String[] args) {
+        int total = 75;
+        int capacity = 30;
+        int cargo = 2;
+
+        System.out.println((double)total / capacity * cargo);
+    }
+
+}
